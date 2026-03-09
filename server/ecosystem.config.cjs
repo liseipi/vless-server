94bab16a1409c906e56b5072a09192c9dc584036
oli <oli@qq.com>
2026-03-09 23:02:43 +0800

    fix

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -16,6 +16,23 @@ module.exports = {
                 HOST: '0.0.0.0',
                 UUID: '55a95ae1-4ae8-4461-8484-457279821b40'
             }
+        },
+        {
+            name: 'vless-server-ssl',
+            script: './server.js',
+            // 不要使用 cluster 模式，因为 WebSocket 不适合多进程
+            instances: 1,
+            exec_mode: 'fork',
+            // 自动重启
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+                PORT: 2054,
+                HOST: '0.0.0.0',
+                UUID: '55a95ae1-4ae8-4461-8484-457279821b40'
+            }
         }
     ]
 };
